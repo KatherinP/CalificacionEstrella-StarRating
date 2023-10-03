9bcb5a486d4884cce66fd0af29d2b6d7160de6fa
Yulieth Caterin <ycpaezch@gmail.com>
2023-10-03 14:57:53 -0500

    Nuevas adiciones - Se agrega JS para que recorra de nuevo NpdeList - Se agregan estilos

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const stars = document.querySelectorAll(".stars i");
 
 // loop through the "stars" NodeList/recorrer la lista de nodos "estrellas"
 stars.forEach((star, index1) => {
-    // add an event listener that runs a function when the "click" event is triggered/agregue un detector de eventos que ejecute una función cuando se active el evento "clic"
-    star.addEventListener("click",() -> {
-        console.log(index1);
+    // add an event listener that runs a function when the "click" event is triggered/agregue un detector de eventos que ejecute una función cuando se active el evento "click"
+    star.addEventListener("click",() => {
+        // loop through the "stars" NodeList again/recorra de nuevo la lista de nodos "estrellas"
+        stars.forEach((star, index2) => {
+        //     add the "active" class to the clicked star and any stars with a lower index
+        // and remove the "active" class from any stars with a higher index/
+        //     agregue la clase "activa" a la estrella en la que se hizo click y a cualquier estrella con un índice más bajo
+        // y eliminar la clase "activa" de cualquier estrella con un índice más alto
+        index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+        });
     });
 });
